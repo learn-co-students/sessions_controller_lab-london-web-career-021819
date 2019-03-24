e4aeee73592050bbf2e6344e396e107af27ae910
CalieR <calielrushton@gmail.com>
2019-03-24 13:50:39 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+   # redirect to the login form if no name entered 
+  def create 
+    if params[:name] == nil || params[:name].empty?
+      redirect_to '/sessions/new'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  # user clicks logout link on homepage, delete user and return to homepage
+  def destroy
+      session.delete :name
+      redirect_to '/sessions/new'
+  end
+end
